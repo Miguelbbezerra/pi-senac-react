113a621a6c70b55f3f439699a5f39ba8811fe728
Carlo Miguel <carlosmiguelcm820@gmail.com>
2024-08-27 19:11:10 -0400

    publicity sitee

--- src/config/route/index.tsx
+++ src/config/route/index.tsx
@@ -8,10 +8,12 @@ import Perfil from "../../pages/perfil";
 import Podologo from "../../pages/podologo";
 import FichaAnamnese from "../../pages/fichaAnamnese";
 import PrivateRoute from "./privateRoute";
+import Publicidade from "../../pages/publicidade/publicidade";
 
 
 export const router = createBrowserRouter(createRoutesFromElements(
     <Route>
+        <Route path="/uo-dds" element={<Publicidade />} />
         <Route path="/" element={<Login />} />
         <Route element={<PrivateRoute redirectTo="/" />}>
             <Route path="/admin" element={<Root />}>
